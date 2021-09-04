e1c35acb4b7bba7848754330fd7775c48104237d
Andremachado82 <andrels.machado82@gmail.com>
2021-09-04 02:55:32 -0300

    Update class Client

--- src/main/java/com/saloon/seed/models/Client.java
+++ src/main/java/com/saloon/seed/models/Client.java
@@ -1,23 +1,29 @@
 package com.saloon.seed.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import javax.persistence.*;
 import java.io.Serializable;
 
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class Client implements Serializable {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-    @Id
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+	
     private String name;
     private String phone;
     private String email;
