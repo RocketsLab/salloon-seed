45743124bb6d70ce827b0c0fc460eb12907ef92f
Andremachado82 <andrels.machado82@gmail.com>
2021-09-04 00:52:20 -0300

    First commit

---
+++ src/main/java/com/saloon/seed/SallonSeedApplication.java
@@ -0,0 +1,13 @@
+package com.saloon.seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SallonSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SallonSeedApplication.class, args);
+	}
+
+}
